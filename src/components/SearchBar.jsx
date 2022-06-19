1d7efbcb97cbc8b2cd32e663f0648996032ac445
RamiroDiaz22 <diazbravoramiro@gmail.com>
2022-06-18 22:53:09 -0300

    commit

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,24 +1,22 @@
 import React, { useState } from "react";
-import { BiSearch } from 'react-icons/bi'
-import './SearchBar.css'
+import { BiSearch } from "react-icons/bi";
+import "./SearchBar.css";
 
-export default function SearchBar({onSearch}) {
+export default function SearchBar({ onSearch }) {
   const [city, setCity] = useState("");
 
-  const handleSubmit = function(e) {
+  const handleSubmit = function (e) {
     e.preventDefault();
     onSearch(city);
-    setCity("")
-  }
+    setCity("");
+  };
 
-  const handleChange = function(e) {
-    setCity(e.target.value)
-  }
-  
+  const handleChange = function (e) {
+    setCity(e.target.value);
+  };
+
   return (
-    <form
-      className="busqueda-serachBar"
-      onSubmit={handleSubmit}>
+    <form className="busqueda-serachBar" onSubmit={handleSubmit}>
       <input
         className="barra-serachBar"
         type="text"
@@ -27,11 +25,10 @@ export default function SearchBar({onSearch}) {
         name="buscar"
         onChange={handleChange}
       />
-      <input
-        className="btn-serachBar"
-        type="submit"
-        value="Search" />
-      <button type="submit" className="btn__searchbar"><BiSearch /></button>
+      <input className="btn-serachBar" type="submit" value="Search" />
+      <button type="submit" className="btn__searchbar">
+        <BiSearch />
+      </button>
     </form>
   );
 }
